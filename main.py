faf3263e78d970bdb8527a0577662267dcdcc5d2
Kubiczeek <jakubakubek@gmail.com>
2024-10-25 20:28:50 +0200

    feat(automatic): add automatic periodic image capturing

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import cv2 as cv
 import datetime as dt
 
+automatic = False
 folder = "images/"
 image_count = 0
 
@@ -24,7 +25,7 @@ def set_count_by_images() -> None:
     global image_count
     image_count = len(os.listdir(folder))
 
-def main() -> None:
+def main_manual() -> None:
     result, image = cam.read()
     if result:
         cv.imshow("Image", image)
@@ -41,12 +42,35 @@ def main() -> None:
             break
         elif key == ord("s"):
             save_image(image)
-            main()
+            main_manual()
             break
         elif key == ord("r"):
-            main()
+            main_manual()
             break
+
+# automatic mode saves images automatically without user input every 5 minutes
+def main_automatic(time_delta : int = 5*60*1000) -> None:
+    while True:
+        result, image = cam.read()
+        if result:
+            save_image(image)
+        else:
+            print("Failed to read image from camera")
+        cv.waitKey(time_delta)  # Wait for 5 minutes (300000 milliseconds)
 
 if __name__ == "__main__":
     set_count_by_images()
-    main()
+    print("Automatic mode? (y/n)")
+    automatic = input().lower() == "y"
+    print("How many milliseconds between each image? (default 5 minutes)")
+    while True:
+        try:
+            time_delta = int(input())
+            break
+        except ValueError:
+            print("Invalid input, please enter an integer")
+
+    if automatic:
+        main_automatic(time_delta)
+    else:
+        main_manual()
